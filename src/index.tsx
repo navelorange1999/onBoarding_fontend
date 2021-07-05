a574eb3fb3495c1c1cc711ecc2a4fbe9b3a9d856
q.chen <q.chen@qchendeMacBook-Pro.local>
2021-07-05 15:19:36 +0800

    :art:  Change index.css and loginPage's import way

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
+import React from "react";
 import ReactDOM from 'react-dom';
 import '@shopify/polaris/dist/styles.css';
-import enTranslations from '@shopify/polaris/locales/en.json';
-import { AppProvider } from '@shopify/polaris';
 
-import './index.css';
-import App from './App';
+import '@/index.css';
+import App from '@/App';
 
 ReactDOM.render(
-  <AppProvider i18n={enTranslations}>
-    <App />
-  </AppProvider>,
+  <App />,
   document.getElementById('root')
 );
 
